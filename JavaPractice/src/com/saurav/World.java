4baabcef2731fbffd67f3a043afae4ae38a3618a
Saurav Sharma <saurav.sharma@imaginea.com>
2018-11-28 17:04:28 +0530

    Test program

--- JavaPractice/src/com/saurav/World.java
+++ JavaPractice/src/com/saurav/World.java
@@ -5,5 +5,7 @@ public class World {
     public static void world (String [] args) {
 
         System.out.println("This is a test statement.");
+
+        System.out.println("This is another staement.")
     }
 }
